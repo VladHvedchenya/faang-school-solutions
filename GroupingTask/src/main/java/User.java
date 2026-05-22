e069945d4a4df8795fbd72258e5e9653a7792141
Vlad Hvedchenya <hvdvlad@gmail.com>
2026-05-22 15:46:31 +0300

    the grouping task solution

---
+++ GroupingTask/src/main/java/User.java
@@ -0,0 +1,28 @@
+public class User {
+    private final String name;
+    private final int age;
+    private final String workplace;
+    private final String address;
+
+    public User(String name, int age, String workplace, String address){
+        if (name == null)
+            throw new NullPointerException("The name shouldn't be null");
+        if (name.isBlank())
+            throw new IllegalArgumentException("The name can't be empty or contain only spaces");
+        if (age < 0 || age > 100)
+            throw new IllegalArgumentException("The age is invalid!");
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + " возраст " + age + " место работы " + workplace + " адрес " + address;
+    }
+}
