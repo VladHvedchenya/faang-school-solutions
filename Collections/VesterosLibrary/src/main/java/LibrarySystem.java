272691492e16dc643a668f1605fdda78f22a4c4d
Vlad Hvedchenya <hvdvlad@gmail.com>
2026-05-25 14:23:38 +0300

    library system added

---
+++ Collections/VesterosLibrary/src/main/java/LibrarySystem.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LibrarySystem {
+    private final Map<Book, String> map = new HashMap<>();
+
+    public boolean addBook(Book book, String location){
+        if (book == null)
+            return false;
+
+        if (map.containsKey(book)){
+            System.out.println("The book is already on the bookshelf");
+            return false;
+        }
+
+        map.put(book, location);
+        return true;
+    }
+
+    public boolean removeBook(Book book){
+        return map.remove(book) != null;
+    }
+
+    public String findBook(Book book){
+        return map.getOrDefault(book, "is not found");
+    }
+
+    public void printAllBook(){
+        for (Map.Entry<Book, String> entry : map.entrySet()){
+            System.out.println(entry.getKey().toString() + " is on the " + entry.getValue() + " shelf");
+        }
+    }
+}
