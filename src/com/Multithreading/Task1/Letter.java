53a193e517b44fbeadd3dbb9836681701fa37f89
Vlad Hvedchenya <hvdvlad@gmail.com>
2026-06-04 22:33:25 +0300

    added multithreading-task1

---
+++ src/com/Multithreading/Task1/Letter.java
@@ -0,0 +1,14 @@
+package com.Multithreading.Task1;
+
+public class Letter {
+    private final String name;
+
+    public Letter(String name){
+        //null & empty checks
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
