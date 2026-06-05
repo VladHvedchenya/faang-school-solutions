37ee7a143e67f5fb0965e7bdd26766574b35fe1f
Vlad Hvedchenya <hvdvlad@gmail.com>
2026-06-05 12:51:10 +0300

    added multithreading-task3

---
+++ src/com/Multithreading/Task3/WeasleyFamily.java
@@ -0,0 +1,36 @@
+package com.Multithreading.Task3;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class WeasleyFamily {
+    private final List<Chore> chores;
+
+    public WeasleyFamily(List<Chore> chores){
+        //null and empty checks for list type!
+        this.chores = chores;
+    }
+
+    public void work(){
+        ExecutorService executor = Executors.newCachedThreadPool();
+        chores.stream()
+                .map(ChoreRunnable::new)
+                .forEach(executor::submit);
+
+        executor.shutdown();
+
+        try{
+            if(!executor.awaitTermination(10, TimeUnit.MINUTES)){
+                System.err.println("Уизли не успели сделать уборку вовремя!");
+            }
+        }
+        catch (InterruptedException e){
+            System.err.println("Главный поток прерван");
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("\n[МАГИЯ] Все домашние дела Перси успешно автоматизированы и выполнены!");
+    }
+}
